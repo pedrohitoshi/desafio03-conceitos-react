ba179188adac82a0150da1cb73f1fabe1491699b
Pedro Hitoshi <pedrohitoshi@gmail.com>
2020-12-26 15:41:10 -0300

    implementar listagem e adição dos repositórios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    
+    const response = await api.post('repositories', {
+      title: "Desafio 03 React",
+      url: "http://github.com/...",
+      techs: ["Node.js", "React"]
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +32,14 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
+        
+        {repositories.map(repository => 
+        <li key={repository.id}>{repository.title}
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
-        </li>
+        </li>)}
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
